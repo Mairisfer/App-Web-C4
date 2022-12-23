dbb8a518a4313e49e0a27375a8ae9f1fa45df0ca
Maira F Amaya <Maira.F.Amaya@skf.com>
2022-12-23 16:37:51 -0500

    Transaction in clientPanel

--- frontend/src/views/panel/ClientPanel.jsx
+++ frontend/src/views/panel/ClientPanel.jsx
@@ -54,4 +54,21 @@ function Dashboard() {
   );
 }
 
+function Transaction(props) {
+  const { data } = props;
+  const { idReserv, dateReserv, dateRequest, status } = data;
+
+  return (
+    <div className="transaction grid">
+      <p className="from">
+        <b>Numero de Reserva:</b> {idReserv}
+      </p>
+      <p className="date">* Fecha de Reserva: {dateReserv}</p>
+      <br></br>
+      <p className="date">* Fecha de Solicitud: {dateRequest}</p>
+      <p className="date">{status}</p>
+    </div>
+  );
+}
+
 export default ClientPanel;
